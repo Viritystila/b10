b1fbd21f9d4961ce6c48f125e7719d7de4dc47fc
Mikael Reponen <mikael.reponen@gmail.com>
2018-07-31 00:55:21 +0300

    initial commit

---
+++ b10.clj
@@ -0,0 +1,105 @@
+(ns b10 (:use [overtone.live]))
+
+(do
+  ;Global pulses
+  (do
+    (defonce root-trg-bus (control-bus)) ;; global metronome pulse
+    (defonce root-cnt-bus (control-bus)) ;; global metronome count
+    (defonce beat-trg-bus (control-bus)) ;; beat pulse (fraction of root)
+    (defonce beat-cnt-bus (control-bus)) ;; beat count
+    (def BEAT-FRACTION "Number of global pulses per beat" 30)
+    )
+
+  (do
+    (defsynth root-trg [rate 100]
+      (out:kr root-trg-bus (impulse:kr rate)))
+
+    (defsynth root-cnt []
+      (out:kr root-cnt-bus (pulse-count:kr (in:kr root-trg-bus))))
+
+    (defsynth beat-trg [div BEAT-FRACTION]
+      (out:kr beat-trg-bus (pulse-divider (in:kr root-trg-bus) div)))
+
+    (defsynth beat-cnt []
+      (out:kr beat-cnt-bus (pulse-count (in:kr beat-trg-bus)))))
+
+  (do
+    (def r-trg (root-trg))
+    (def r-cnt (root-cnt [:after r-trg]))
+    (def b-trg (beat-trg [:after r-trg]))
+    (def b-cnt (beat-cnt [:after b-trg]))
+    (ctl r-trg :rate 10)
+    )
+
+                                    ;Buses
+  (do
+                                    ;Control
+    (defonce cbus1 (control-bus 1))
+    (defonce cbus2 (control-bus 1))
+    (defonce cbus3 (control-bus 1))
+    (defonce cbus4 (control-bus 1))
+    (defonce cbus5 (control-bus 1))
+    (defonce cbus6 (control-bus 1))
+    (defonce cbus7 (control-bus 1))
+    (defonce cbus8 (control-bus 1))
+    (defonce cbus9 (control-bus 1))
+    (defonce cbus10 (control-bus 1))
+    (defonce cbus11 (control-bus 1))
+    (defonce cbus12 (control-bus 1))
+    (defonce cbus13 (control-bus 1))
+    (defonce cbus14 (control-bus 1))
+    (defonce cbus15 (control-bus 1))
+    (defonce cbus16 (control-bus 1))
+
+    ;Audio
+    (defonce abus1 (audio-bus))
+    (defonce abus2 (audio-bus))
+    (defonce abus3 (audio-bus))
+    (defonce abus4 (audio-bus))
+    (defonce abus5 (audio-bus))
+    (defonce abus6 (audio-bus))
+    (defonce abus7 (audio-bus))
+    (defonce abus8 (audio-bus))
+    (defonce abus9 (audio-bus))
+    (defonce abus10 (audio-bus))
+    (defonce abus11 (audio-bus))
+    (defonce abus12 (audio-bus))
+    (defonce abus13 (audio-bus))
+    (defonce abus14 (audio-bus))
+    (defonce abus15 (audio-bus))
+    (defonce abus16 (audio-bus))
+
+
+                                        ;Groups 
+    (defonce main-g (group "main bus"))
+    (defonce early-g (group "early bus" :head main-g))
+    (defonce later-g (group "late bus" :after early-g))
+    )
+                                        ;Buffers
+    (defonce buffer-4-1 (buffer 4))
+    (defonce buffer-4-2 (buffer 4))
+    (defonce buffer-4-3 (buffer 4))
+    (defonce buffer-4-4 (buffer 4))
+  
+    (defonce buffer-8-1 (buffer 8))
+    (defonce buffer-8-2 (buffer 8))
+    (defonce buffer-8-3 (buffer 8))
+    (defonce buffer-8-4 (buffer 8))
+  
+    (defonce buffer-16-1 (buffer 16))
+    (defonce buffer-16-2 (buffer 16))
+    (defonce buffer-16-3 (buffer 16))
+    (defonce buffer-16-4 (buffer 16))
+    
+    (defonce buffer-32-1 (buffer 32))
+    (defonce buffer-32-2 (buffer 32))
+    (defonce buffer-32-3 (buffer 32))
+    (defonce buffer-32-4 (buffer 32))
+
+
+  )
+
+                                    ;Synths
+
+
+
