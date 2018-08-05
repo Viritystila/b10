b6f11b2421c38eb7c7eb1fd442f145c980ad46ca
Mikael Reponen <mikael.reponen@gmail.com>
2018-08-05 11:53:47 +0300

    synhesis experients with buses and groups

--- b10.clj
+++ b10.clj
@@ -1,4 +1,4 @@
-(ns b10 (:use [overtone.live]))
+(ns b10 (:use [overtone.live]) (:require [shadertone.tone :as t]) )
 
 (do
   ;Global pulses
@@ -70,7 +70,7 @@
     (defonce abus16 (audio-bus))
 
 
-                                        ;Groups 
+                                        ;Groups
     (defonce main-g (group "main bus"))
     (defonce early-g (group "early bus" :head main-g))
     (defonce later-g (group "late bus" :after early-g))
@@ -80,26 +80,70 @@
     (defonce buffer-4-2 (buffer 4))
     (defonce buffer-4-3 (buffer 4))
     (defonce buffer-4-4 (buffer 4))
-  
+
     (defonce buffer-8-1 (buffer 8))
     (defonce buffer-8-2 (buffer 8))
     (defonce buffer-8-3 (buffer 8))
     (defonce buffer-8-4 (buffer 8))
-  
+
     (defonce buffer-16-1 (buffer 16))
     (defonce buffer-16-2 (buffer 16))
     (defonce buffer-16-3 (buffer 16))
     (defonce buffer-16-4 (buffer 16))
-    
+
     (defonce buffer-32-1 (buffer 32))
     (defonce buffer-32-2 (buffer 32))
     (defonce buffer-32-3 (buffer 32))
     (defonce buffer-32-4 (buffer 32))
-
-
   )
 
+                                        ;Control synths
+(defsynth sin-out [freq 1 out-control-bus 0]
+  (out:kr out-control-bus (+ (in:kr freq) (sin-osc:kr (in:kr freq)))))
+
+(def sin-out_1 (sin-out [:head early-g] cbus5 cbus6))
+
+(kill sin-out_1)
                                     ;Synths
+(defsynth sin-wave [amp 1 freq 22 phase 0 out-bus 0]
+  (let [amp_in (in:kr amp)
+        freq_in (in:kr freq)
+        phase_in (in:kr phase)
+        src (sin-osc freq_in phase_in 0)]
+    (out out-bus (* amp_in src))))
+
+(def sin-wave_1 (sin-wave [:head early-g] :amp cbus1 :freq cbus2 :phase cbus3 :out-bus abus1))
+
+(def sin-wave_2 (sin-wave [:tail early-g] :amp cbus4 :freq cbus6 :phase 0 :out-bus abus2))
+
+(ctl sin-wave_2 :phase cbus6 :freq cbus6)
+
+(kill sin-wave_1)
+
+(control-bus-set! cbus1 1)
+
+(control-bus-set! cbus2 100)
+
+(control-bus-set! cbus4 1.0)
+
+(control-bus-set! cbus6 (* Math/PI 0.0))
+
+(control-bus-set! cbus5 100)
+
+(control-bus-get cbus6)
+
+(pp-node-tree)
 
 
 
+(kill 85)
+
+(defsynth mixer [in-bus1 0 in-bus2 0 amp 1]
+  (out 0 (pan2 (*  (+ (in in-bus1) (in in-bus2))  amp))))
+
+(def mixer1 (mixer [:tail early-g] abus1 abus2 1))
+
+(stop)
+
+(kill 47)
+(kill mixer1)
